23fc3fccc8fe6fab25307a43f09c953bbbd7b32f
Nairisz <norizzaty.norsupardi@gmail.com>
2025-10-05 13:38:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ with col2:
     st.write("**Phone:** (60) 19-223 8570")
     st.write("**LinkedIn:** [linkedin.com/in/norizzaty-norsupardi](https://linkedin.com/in/norizzaty-norsupardi)")
 
+st.markdown("""
+<hr style="border:1.8px solid #0b83b3; margin: 15px 0;">
+""", unsafe_allow_html=True)
+
 st.header("Education")
 
 st.markdown("""    
@@ -43,28 +47,38 @@ st.markdown("""
 *2014 - 2015*   
 """)
 
+st.markdown("""
+<hr style="border:1.8px solid #0b83b3; margin: 15px 0;">
+""", unsafe_allow_html=True)
+
 st.header("Work Experience")
 
 st.markdown("""
-**Video Editor — Internship**  
-*Talentiv International Sdn Bhd*    
-*Jan 2020 - Mar 2020*
-- Support on-site production and deliver post-production editing for dramas and television programmes.   
+**Tearista — Part Time**  
+*Tealive*    
+*Apr 2022 - Oct 2022*
+- Provided excellent customer service and food handling. 
 
----
+---             
 
 **Video Editor — Contract**  
 *Talentiv International Sdn Bhd*    
 *Jul 2020 - Jun 2021*
-- Edited dramas and TV programmes, and supported production with equipment handling and proposal drafting.   
+- Edited dramas and TV programmes, and supported production with equipment handling and proposal drafting.  
+            
+--- 
+            
+**Video Editor — Internship**  
+*Talentiv International Sdn Bhd*    
+*Jan 2020 - Mar 2020*
+- Support on-site production and deliver post-production editing for dramas and television programmes.           
 
----
 
-**Tearista — Part Time**  
-*Tealive*    
-*Apr 2022 - Oct 2022*
-- Provided excellent customer service and food handling.                
 """)
+
+st.markdown("""
+<hr style="border:1.8px solid #0b83b3; margin: 15px 0;">
+""", unsafe_allow_html=True)
 
 st.header("Achievements")
 st.markdown("""
@@ -75,6 +89,9 @@ st.markdown("""
 - Achieved Dean's List for 3 semesters.
 """)
 
+st.markdown("""
+<hr style="border:1.8px solid #0b83b3; margin: 15px 0;">
+""", unsafe_allow_html=True)
 
 st.header("Skills")
 st.markdown("""
@@ -84,48 +101,56 @@ st.markdown("""
 - UI/UX Design
 """)
 
+st.markdown("""
+<hr style="border:1.8px solid #0b83b3; margin: 15px 0;">
+""", unsafe_allow_html=True)
+
 st.header("Projects")
 st.markdown("""
 ### Year 1
-##### Pet Grooming Service 
+##### 1. Pet Grooming Service 
 - Developing a booking system for pet grooming services using Python.  
 
 ---
 
 ### Year 2
-##### Student Supervision Management System
+##### 1. Student Supervision Management System
 - Creating an application for student management (tracking class, attendance, and performance) and database for it.  
 
-##### Library Management System     
+##### 2. Library Management System     
 - Developing a library management system with authentication, borrowing, and returning features and inventory updates using Java.  
 
-##### Bus Schedule App      
+##### 3. Bus Schedule App      
 - Designing a bus schedule app with real-time updates and reminder using Flutter and Dart.  
 
-##### Smart Crosswalk   
+##### 4. Smart Crosswalk   
 - Creating an IoT project for smart crosswalk system with sensors to detect pedestrian and traffic light control.  
 
 ---
 
 ### Year 3
-##### VendiMed  
+##### 1. VendiMed  
 - Developing a vending machine prototype that dispenses medications while integrated with an app for booking and scheduled pickup.  
 
-##### The Healthcare Disease Prediction for Diabetes  
+##### 2. The Healthcare Disease Prediction for Diabetes  
 - Creating a healthcare disease prediction system focusing on diabetes using Fuzzy Logic with several combinations of SVM, RF, and LSTM.  
 
-##### Regression For Flood Forecasting: Case Study in Gong Kedak, Terengganu  
+##### 3. Regression For Flood Forecasting: Case Study in Gong Kedak, Terengganu  
 - Building a flood forecasting system focusing on Gong Kedak, Terengganu using regression analysis.  
 
-##### Analysis Movie Rating  
+##### 4. Analysis Movie Rating  
 - Analyzing movie ratings using Hadoop and MapReduce to find patterns and trends.  
 
-##### FBMKLCI Stock Price Prediction using LSTM  
+##### 5. FBMKLCI Stock Price Prediction using LSTM  
 - Predicting stock prices using LSTM neural networks and finding the best tuning parameters.  
 
-##### Predicting the Risk of Heart Disease using Multiple Machine Learning Algorithms  
+##### 6. Predicting the Risk of Heart Disease using Multiple Machine Learning Algorithms  
 - Developing a heart disease risk prediction model using several ML algorithms and comparing accuracy results.  
 """)
+
+st.markdown("""
+<hr style="border:1.8px solid #0b83b3; margin: 15px 0;">
+""", unsafe_allow_html=True)
 
 st.header("Activities")
 st.markdown("""
@@ -133,6 +158,10 @@ st.markdown("""
 - Joined the university's PRS (Pembimbing Rakan Siswa) club (2023/2024).
 - Joined for PRS Exco Publicity (2023/2024).
 """)
+
+st.markdown("""
+<hr style="border:1.8px solid #0b83b3; margin: 15px 0;">
+""", unsafe_allow_html=True)
 
 st.header("References")
 col1, col2, col3 = st.columns(3)
